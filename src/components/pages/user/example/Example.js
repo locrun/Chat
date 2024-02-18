f1fbb8d7aad26946c1f440c25905b3ff40891344
Runar <github.runar@gmail.com>
2024-02-18 11:16:38 +0300

    fix: example page

--- src/components/pages/user/example/Example.js
+++ src/components/pages/user/example/Example.js
@@ -1,14 +1,37 @@
+import React from 'react';
+import * as ReactBootstrap from 'react-bootstrap';
 import ProfileUserCard from 'components/ProfileUserCard/ProfileUserCard';
 import { ServiceCard } from 'components/ServiceCard/ServiceCard';
-import React from 'react';
+import MessageStarting from '../../../message-starting/MessageStarting';
 
 const Example = () => {
   return (
-    <>
+    <div>
       <h1>Example Page</h1>
+      <ReactBootstrap.Dropdown>
+        <ReactBootstrap.Dropdown.Toggle variant="falcon-default">
+          Dropdown
+        </ReactBootstrap.Dropdown.Toggle>
+        <ReactBootstrap.Dropdown.Menu className="py-2">
+          <ReactBootstrap.Dropdown.Item href="#/action-1">
+            Action
+          </ReactBootstrap.Dropdown.Item>
+          <ReactBootstrap.Dropdown.Item href="#/action-2">
+            Another action
+          </ReactBootstrap.Dropdown.Item>
+          <ReactBootstrap.Dropdown.Item href="#/action-3">
+            Something else
+          </ReactBootstrap.Dropdown.Item>
+          <ReactBootstrap.Dropdown.Divider />
+          <ReactBootstrap.Dropdown.Item href="#/action-4">
+            Separated link
+          </ReactBootstrap.Dropdown.Item>
+        </ReactBootstrap.Dropdown.Menu>
+      </ReactBootstrap.Dropdown>
+      <MessageStarting />
       <ProfileUserCard />
       <ServiceCard />
-    </>
+    </div>
   );
 };
 
