44b857788c25ee539f42c79b22f195eb53af0924
Bur_Bull <98647011+BullatBor@users.noreply.github.com>
2024-02-20 18:39:28 +0700

    add need select

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -8,6 +8,7 @@ import MessageStarting from 'components/message-starting/MessageStarting';
 import { ServiceCards } from 'components/ServiceCards/ServiceCards';
 import { checkbox } from 'data/checkboxses';
 import Search from 'components/doc-components/Search';
+import { Dropdowns } from 'components/dropdown';
 
 export const Example = () => {
   const [checkboxses, setCheckboxes] = useState(checkbox);
@@ -25,6 +26,17 @@ export const Example = () => {
   const handleChangeRadio = selected => {
     console.log(selected.value);
   };
+
+  const DROPDOWNITEMS /*: TItems[]*/ = [
+    { item: 'Link 1', href: 'google.com', active: true, quantity: 2 },
+    { item: 'Custom Text', eventKey: '1', itemAs: 'button', quantity: 45 },
+    { hr: true },
+    { item: 'Text', itemAs: 'text', quantity: 32 },
+    { item: 'Another Text', eventKey: '2', itemAs: 'button', quantity: 77 }
+  ];
+
+  const [month, setMonth] = React.useState(0);
+
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       <ProfileUserCard />
@@ -45,6 +57,21 @@ export const Example = () => {
       />
       <div style={{ width: '100%' }}>
         <Search />
+      </div>
+      <div style={{ width: '500px', display: 'flex', gap: '10px' }}>
+        <Dropdowns
+          type={'dropdownBtnCode'}
+          items={DROPDOWNITEMS}
+          title="Chat theme"
+          onSelect={console.log}
+        />
+        <Dropdowns
+          type={'dropdownBtnCode'}
+          items={DROPDOWNITEMS}
+          title="Chat theme"
+          quantity={54}
+          onSelect={console.log}
+        />
       </div>
     </div>
   );
