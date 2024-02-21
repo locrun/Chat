4b1514be3105db2dd26f501552a7aa011d7c9712
Runar N <github.runar@gmail.com>
2024-02-21 22:02:50 +0300

    create student chat page (#18)
    * create student chat page
    
    * adaptive chat

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import NewСonversation from '../pages/NewСonversation/NewСonversation';
-import { AdminMessages } from '../pages/AdminMessages/AdminMessages';
 import { Example } from '../pages/Example/Example';
+import NewСonversation from '../pages/NewСonversation/NewСonversation';
+import { AdminChat } from '../pages/AdminChat/AdminChat';
+import { StudentChat } from 'pages/StudentChat/StudentChat';
+
 const RoutesPage = () => {
   return (
     <Routes>
       <Route path="/" element={<Example />} />
-      <Route path="/admin-messages" element={<AdminMessages />} />
+      <Route path="/admin-chat" element={<AdminChat />} />
+      <Route path="/student-chat" element={<StudentChat />} />
       <Route path="/new-conversation" element={<NewСonversation />} />
     </Routes>
   );
