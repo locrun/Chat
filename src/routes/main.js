c13eacdc222ccd71c8e0a252c4b8eb5d18709843
Runar <github.runar@gmail.com>
2024-02-22 08:25:54 +0300

    rename page

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Example } from '../pages/Example/Example';
+import NewChat from '../pages/NewChat/NewChat';
 import { StudentProfile } from 'pages/StudentProfile/StudentProfile';
 import { AdminChat } from '../pages/AdminChat/AdminChat';
 import { StudentChat } from 'pages/StudentChat/StudentChat';
-import NewСonversation from '../pages/NewСonversation/NewСonversation';
 
 const RoutesPage = () => {
   return (
@@ -12,7 +12,7 @@ const RoutesPage = () => {
       <Route path="/" element={<Example />} />
       <Route path="/admin-chat" element={<AdminChat />} />
       <Route path="/student-chat" element={<StudentChat />} />
-      <Route path="/new-conversation" element={<NewСonversation />} />
+      <Route path="/new-chat" element={<NewChat />} />
       <Route path="/student-profile" element={<StudentProfile />} />
     </Routes>
   );
