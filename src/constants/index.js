e724d93b615043419e1603a17ca48ce160926ffe
R-serban <85891583+R-serban@users.noreply.github.com>
2024-02-23 22:59:54 +0500

    add constants (#25)
    * add constants
    
    * fix export
    
    * rename file

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+
+const MAX_FILE_SIZE_MB = 20;
+const SUPPORTED_FILE_FORMATS = ['.xls', '.xlsx', '.doc', '.docx', '.pdf', '.jpg', '.png', '.pptx', '.mp4', '.avi', '.3gpp'];
+
+const KEYCLOAK_IP = 'your_keycloak_ip';
+const PLATFORM_URL = 'your_platform_url';
+
+
+export default {
+    MAX_FILE_SIZE_MB,
+    SUPPORTED_FILE_FORMATS,
+    KEYCLOAK_IP,
+    PLATFORM_URL,
+};
