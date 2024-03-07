759497b3a6164cbb82be8a27e34cd32ed00d618f
Runar N <github.runar@gmail.com>
2024-03-07 16:46:11 +0300

    search request (#38)

--- src/components/app/chat/sidebar/ChatContactSearch.js
+++ src/components/app/chat/sidebar/ChatContactSearch.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { getClientChats } from 'api/routes/clientChat';
+import { ChatContext } from 'context/Context';
 
-const ChatContactsSearch = () => (
-  <Form className="contacts-search-wrapper" onSubmit={e => e.preventDefault()}>
-    <Form.Group className="mb-0 position-relative d-md-none d-lg-block w-100 h-100">
-      <Form.Control
-        style={{ boxShadow: 'none' }}
-        className="chat-contacts-search border-0 h-100"
-        placeholder="Искать контакт..."
-        size="sm"
-        onChange={e => console.log(e.target.value)}
-      />
-      <FontAwesomeIcon icon="search" className="contacts-search-icon" />
-    </Form.Group>
-    <Button
-      variant="transparent"
-      size="sm"
-      className="d-none d-md-inline-block d-lg-none"
-      type="submit"
+const ChatContactsSearch = () => {
+  const { threadsDispatch } = useContext(ChatContext);
+  // const [value, setValue] = useState('');
+
+  const handleSearchTopic = async e => {
+    const value = e.target.value;
+
+    try {
+      const { data } = await getClientChats({ search: value });
+
+      threadsDispatch({
+        type: 'SET_DIALOGS',
+        payload: data.results
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Form
+      className="contacts-search-wrapper"
+      onSubmit={e => e.preventDefault()}
     >
-      <FontAwesomeIcon icon="search" className="fs-10" />
-    </Button>
-  </Form>
-);
+      <Form.Group className="mb-0 position-relative d-md-none d-lg-block w-100 h-100">
+        <Form.Control
+          style={{ boxShadow: 'none' }}
+          className="chat-contacts-search border-0 h-100"
+          placeholder="Искать контакт..."
+          size="sm"
+          onChange={e => handleSearchTopic(e)}
+        />
+        <Button
+          variant="transparent"
+          size="sm"
+          className="d-md-inline-block d-lg-none"
+          type="submit"
+          onClick={() => handleSearchTopic()}
+        >
+          {/* <FontAwesomeIcon icon="search" className="fs-10" /> */}
+          <FontAwesomeIcon icon="search" className="contacts-search-icon" />
+        </Button>
+      </Form.Group>
+    </Form>
+  );
+};
 
 export default ChatContactsSearch;
