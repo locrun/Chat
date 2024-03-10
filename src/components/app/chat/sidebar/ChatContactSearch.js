78a0832ffe27a3dcbd8a1e4d439d8bdc7301a6bd
Runar N <github.runar@gmail.com>
2024-03-10 17:32:29 +0300

    use Debounce to find contacts (#44)

--- src/components/app/chat/sidebar/ChatContactSearch.js
+++ src/components/app/chat/sidebar/ChatContactSearch.js
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDebounce } from 'hooks/useDebaunce';
 import { getClientChats } from 'api/routes/clientChat';
 import { ChatContext } from 'context/Context';
 import { useKeycloak } from '@react-keycloak/web';
@@ -9,41 +10,49 @@ import keycloakRealmRoles from 'helpers/keycloakRealmRoles';
 import { getCuratorChats } from 'api/routes/curatorChat';
 
 const ChatContactsSearch = () => {
-  const { threadsDispatch } = useContext(ChatContext);
   const { keycloak } = useKeycloak();
-  // const [value, setValue] = useState('');
+  const { threadsDispatch } = useContext(ChatContext);
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
 
-  const handleSearchTopic = async e => {
-    const value = e.target.value;
+  useEffect(() => {
+    const handleSearchTopic = async () => {
+      try {
+        if (
+          checkAllRealmRolesAssigned(keycloak.realmAccess.roles, [
+            keycloakRealmRoles.CHAT_USER
+          ])
+        ) {
+          const { data } = await getClientChats({
+            search: debouncedSearchTerm
+          });
 
-    try {
-      if (
-        checkAllRealmRolesAssigned(keycloak.realmAccess.roles, [
-          keycloakRealmRoles.CHAT_USER
-        ])
-      ) {
-        const { data } = await getClientChats({ search: value });
+          threadsDispatch({
+            type: 'SET_DIALOGS',
+            payload: data.results
+          });
+        } else if (
+          checkAllRealmRolesAssigned(keycloak.realmAccess.roles, [
+            keycloakRealmRoles.CHAT_MANAGER
+          ])
+        ) {
+          const { data } = await getCuratorChats({
+            search: debouncedSearchTerm
+          });
 
-        threadsDispatch({
-          type: 'SET_DIALOGS',
-          payload: data.results
-        });
-      } else if (
-        checkAllRealmRolesAssigned(keycloak.realmAccess.roles, [
-          keycloakRealmRoles.CHAT_MANAGER
-        ])
-      ) {
-        const { data } = await getCuratorChats({ search: value });
-
-        threadsDispatch({
-          type: 'SET_DIALOGS',
-          payload: data.results
-        });
+          threadsDispatch({
+            type: 'SET_DIALOGS',
+            payload: data.results
+          });
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
+
+    handleSearchTopic();
+  }, [debouncedSearchTerm]);
+
   return (
     <Form
       className="contacts-search-wrapper"
@@ -55,14 +64,13 @@ const ChatContactsSearch = () => {
           className="chat-contacts-search border-0 h-100"
           placeholder="Искать контакт..."
           size="sm"
-          onChange={e => handleSearchTopic(e)}
+          onChange={e => setSearchTerm(e.target.value)}
         />
         <Button
           variant="transparent"
           size="sm"
           className="d-md-inline-block d-lg-none"
           type="submit"
-          onClick={() => handleSearchTopic()}
         >
           {/* <FontAwesomeIcon icon="search" className="fs-10" /> */}
           <FontAwesomeIcon icon="search" className="contacts-search-icon" />
