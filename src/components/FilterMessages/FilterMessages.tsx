2d562967d85fb07eda0f96edfae69d02e0abad83
Runar <github.runar@gmail.com>
2024-04-20 11:21:47 +0300

    fix: topic

--- src/components/FilterMessages/FilterMessages.tsx
+++ src/components/FilterMessages/FilterMessages.tsx
@@ -96,7 +96,7 @@ export const FilterMessages = ({
           <Form.Select
             className={s.select}
             onChange={e => handleTypeMessagesChange(e)}
-            defaultValue=""
+            defaultValue="topic"
           >
             {typeMessagesItems.map(item => {
               return (
