e5cae969306c6817f89f7c7615d547e63fbd796d
Runar <github.runar@gmail.com>
2024-05-14 10:09:57 +0300

    fix: delete access-control-allow-credentials

--- src/api/routes/tokenKeycloak.ts
+++ src/api/routes/tokenKeycloak.ts
@@ -13,7 +13,6 @@ export const fetchAccessTokenKeycloak = async () => {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
         'Access-Control-Allow-Headers':
           'Origin, X-Requested-With, Content-Type, Accept, Authorization'
