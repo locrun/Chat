cdaf5f8cdbfd5e9a87f67b7c0e3cca6ee5736e2b
Runar <github.runar@gmail.com>
2024-05-14 10:15:42 +0300

    fix: delete access-control-allow-headers

--- src/api/routes/tokenKeycloak.ts
+++ src/api/routes/tokenKeycloak.ts
@@ -14,8 +14,8 @@ export const fetchAccessTokenKeycloak = async () => {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
-        'Access-Control-Allow-Headers':
-          'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Cache-Control': null,
+        'X-Requested-With': null
       }
     }
   );
